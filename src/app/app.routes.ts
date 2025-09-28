ac775a732d934dc325c0652808ea807b60d87062
Antonio Pagano <iatix1@gmail.com>
2025-09-28 10:23:28 +0200

    hh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,37 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home-component/home-component';
 import { Product } from './components/product/product';
+import { AdminDashboardComponent } from './components/admin-dashboard-component/admin-dashboard-component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    {
-      path: 'admin',
-      loadComponent: () =>
-        import('./components/admin-dashboard-component/admin-dashboard-component').then(m => m.AdminDashboardComponent)
-    },
-    {
-      path: 'profile',
-      loadChildren: () =>
-        import('./components/profile/profile.routes').then((m) => m.PROFILE_ROUTES)
-    },
+  { path: '', component: HomeComponent },
+  { path: 'admin', component: AdminDashboardComponent },
+  /*
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./components/admin-dashboard-component/admin-dashboard-component').then(m => m.AdminDashboardComponent)
+  }
+    
+  ,
+*/
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./components/profile/profile.routes').then((m) => m.PROFILE_ROUTES)
+  },
+  // Wildcard route
+  // { path: '**', component: PageNotFoundComponent } 
 
-    {
-      path: 'product/:id',
-      component: Product,
-    }
+  //Variante con redirect:
+  // { path: 'home', component: HomeComponent },
+  // { path: '**', redirectTo: 'home' } 
 
-  ];
-  
+
+
+  {
+    path: 'product/:id',
+    component: Product,
+  }
+
+];
--- src/app/components/product/product.ts
+++ src/app/components/product/product.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-product',
@@ -7,25 +8,52 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './product.html',
   styleUrl: './product.scss'
 })
-export class Product  implements OnInit {
+export class Product implements OnInit {
   productId!: string;
   referrer!: string;
 
-  constructor(private route: ActivatedRoute) {}
-//http://localhost:4200/product/101?ref=homepage
+  constructor(private route: ActivatedRoute) { }
+
 
   ngOnInit(): void {
+    console.log("dentro ngOnInit");
+
     // Parametro dinamico
+    //http://localhost:4200/product/101
+
     this.route.params.subscribe(params => {
       this.productId = params['id'];
     });
-  
+
+    console.log(this.productId);
+
+
+    /*
+ const id = this.route.snapshot.params['id'];
+this.productId = id;
+console.log(this.productId)
+  */
 
     // Query param
-    //Legge i query params (es. ?ref=homepage) e imposta 
-    // referrer. Se ref non è presente, imposta 'direct'.
-    this.route.queryParams.subscribe(q => {
-      this.referrer = q['ref'] || 'direct';
-    });
+    /*
+        this.route.queryParams.subscribe(q => {
+          this.referrer = q['ref'] || 'direct';
+        });
+        console.log(this.referrer);
+    */
+    //per prendere entrambi 
+    /*
+       combineLatest([
+         this.route.params,
+         this.route.queryParams
+       ]).subscribe(([p, q]) => {
+         const id = p['id'];               // parametro di rotta
+         const ref = q['ref'] || 'direct'; // parametro query
+         console.log('id:', id, 'ref:', ref);
+       });
+   */
+
+
   }
+
 }
